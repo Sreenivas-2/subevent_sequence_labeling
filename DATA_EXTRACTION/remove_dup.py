6a6e292c9cb51354fd082107eff2330e77234375
Sai Sreenivas <52418873+Sreenivas-2@users.noreply.github.com>
2020-05-20 13:27:47 +0530

    Delete remove_dup.py

--- DATA_EXTRACTION/remove_dup.py
+++
@@ -1,9 +0,0 @@
-'''
-	Removes duplicate tweets
-'''
-
-import pandas as pd
-
-df = pd.read_csv('test.csv')
-df = df.drop_duplicates(subset = 'tweet', keep = 'first')
-df.to_csv('final1.csv', index = False)
